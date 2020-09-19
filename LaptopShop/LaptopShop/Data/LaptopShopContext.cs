6e5c71fdcf33be25142c5db50872db2fd80ad2f5
AbdulAziz <>
2020-09-20 02:17:29 +0600

    mid 2

---
+++ LaptopShop/LaptopShop/Data/LaptopShopContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaptopShop.Data
+{
+    public class LaptopShopContext : DbContext
+    {
+        internal object Movies;
+        internal object laptops;
+
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public LaptopShopContext() : base("name=LaptopShopContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<LaptopShop.Models.Laptop> Laptops { get; set; }
+    }
+}
---
+++ LaptopShop/LaptopShop/Models/Laptop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaptopShop.Models
+{
+    public class Laptop
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Core { get; set; }
+        public string Generation { get; set; }
+        public string company { get; set; }
+        public int price { get; set; }
+
+        internal static object Where(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
